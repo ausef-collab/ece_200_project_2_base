30730a79c42be5605a7fd4b34c8a125b56afb5d6
ausef-collab <63215272+ausef-collab@users.noreply.github.com>
2020-04-06 19:56:05 -0400

    Initial commit

---
+++ code/src/PROC.c
@@ -0,0 +1,126 @@
+#include <stdio.h>	/* fprintf(), printf() */
+#include <stdlib.h>	/* atoi() */
+#include <stdint.h>	/* uint32_t */
+
+#include "RegFile.h"
+#include "Syscall.h"
+#include "utils/heap.h"
+#include "elf_reader/elf_reader.h"
+
+int main(int argc, char * argv[]) {
+
+	/*
+	 * This variable will store the maximum
+	 * number of instructions to run before
+	 * forcibly terminating the program. It
+	 * is set via a command line argument.
+	 */
+	uint32_t MaxInstructions;
+
+	/*
+	 * This variable will store the address
+	 * of the next instruction to be fetched
+	 * from the instruction memory.
+	 */
+	uint32_t ProgramCounter;
+
+	/*
+	 * This variable will store the instruction
+	 * once it is fetched from instruction memory.
+	 */
+	uint32_t CurrentInstruction;
+
+	//IF THE USER HAS NOT SPECIFIED ENOUGH COMMAND LINE ARUGMENTS
+	if(argc < 3){
+
+		//PRINT ERROR AND TERMINATE
+		fprintf(stderr, "ERROR: Input argument missing!\n");
+		fprintf(stderr, "Expected: file-name, max-instructions\n");
+		return -1;
+
+	}
+
+     	//CONVERT MAX INSTRUCTIONS FROM STRING TO INTEGER	
+	MaxInstructions = atoi(argv[2]);	
+
+	//Open file pointers & initialize Heap & Regsiters
+	initHeap();
+	initFDT();
+	initRegFile(0);
+
+	//LOAD ELF FILE INTO MEMORY AND STORE EXIT STATUS
+	int status = LoadOSMemory(argv[1]);
+
+	//IF LOADING FILE RETURNED NEGATIVE EXIT STATUS
+	if(status < 0){ 
+		
+		//PRINT ERROR AND TERMINATE
+		fprintf(stderr, "ERROR: Unable to open file at %s!\n", argv[1]);
+		return status; 
+	
+	}
+
+	printf("\n ----- BOOT Sequence ----- \n");
+	printf("Initializing sp=0x%08x; gp=0x%08x; start=0x%08x\n", exec.GSP, exec.GP, exec.GPC_START);
+
+	RegFile[28] = exec.GP;
+	RegFile[29] = exec.GSP;
+	RegFile[31] = exec.GPC_START;
+
+	printRegFile();
+
+	printf("\n ----- Execute Program ----- \n");
+	printf("Max Instruction to run = %d \n",MaxInstructions);
+	fflush(stdout);
+	ProgramCounter = exec.GPC_START;
+	
+	/***************************/
+	/* ADD YOUR VARIABLES HERE */
+	/***************************/
+	uint8_t opcode;
+	unit8_t source_register;	
+	unit8_t target_register;
+	uint16_t target_register;
+	uint16_t immediate;
+	int i;
+	for(i = 0; i < MaxInstructions; i++) {
+
+		//FETCH THE INSTRUCTION AT 'ProgramCounter'		
+		CurrentInstruction = readWord(ProgramCounter,false);
+
+		//PRINT CONTENTS OF THE REGISTER FILE	
+		printRegFile();
+		
+		/********************************/
+		/* ADD YOUR IMPLEMENTATION HERE */
+		/********************************/
+        
+	        printf("currentInstruction: %08x\n", CurrentInstruction);
+		
+
+		opcode = (CurrentInsturction & 0xFC000000) >>20
+		printf("opcode: %u\n", opcode);
+
+		if(opcode == 8){
+			
+			pintf("ADDIU\n");
+			
+			RegFile[target_register] = RegFile[source_register] + ......
+
+			ProgramCounter = ProgramCounter +4;
+		}else if(opcode== ....){
+				
+		//0000 00xx xxxx xxxx xxxx xxxxx xxxx xxxx
+		//1111 1100 0000 0000 0000 00000 0000 0000
+		//F    C    0    0    0    0     0    0
+		//Mask; 0xFC000000
+
+	}   
+
+	//Close file pointers & free allocated Memory
+	closeFDT();
+	CleanUp();
+
+	return 0;
+
+}
